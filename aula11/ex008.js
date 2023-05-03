ddd62e33d1542e2adc6529292757adf2017ef307
mario-moraes <129066014+mario-moraes@users.noreply.github.com>
2023-05-03 13:23:08 -0300

    exercices

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 90;
+console.log(`A velocidade do seu veículo é ${vel} km/h.`);
+if(vel > 60) { // condição simples
+    console.log("Você ultrapassou a velocidade permitida. Multado!");
+}
+console.log("Diriga sempre usando cinto de segurança!");
